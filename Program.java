01d8c603defa4afd2b964d3b7c71db934d906a0f
Saurav Chawla <chawlasaurav18@gmail.com>
2024-05-29 18:27:14 +0530

    Aman Mandape - 1234 Bye feature added successfully.

--- Program.java
+++ Program.java
@@ -1,5 +1,11 @@
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello");
+        bye();
+    }
+
+    static public void bye()
+    {
+        System.out.println("Bye");
     }
 }
